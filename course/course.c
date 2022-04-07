998f5ba9f84d14c4ece7b3165ed558137de581c0
HilaryHe1012 <97636372+HilaryHe1012@users.noreply.github.com>
2022-04-07 10:50:17 -0400

    Add files via upload

---
+++ course/course.c
@@ -0,0 +1,77 @@
+#include "course.h"
+#include <stdlib.h>
+#include <stdio.h>
+// Add students to typedef struct Course
+void enroll_student(Course *course, Student *student)
+{
+  course->total_students++;
+  if (course->total_students == 1) 
+  { // dynamically allocate an array of size one to store 1 student
+    course->students = calloc(1, sizeof(Student));
+  }
+  else 
+  { // reallocate when the number of student is greater than one.
+    course->students = 
+      realloc(course->students, course->total_students * sizeof(Student)); 
+  }
+  course->students[course->total_students - 1] = *student;
+}
+
+// Easy print on the typedef struct Course
+void print_course(Course* course)
+{
+  printf("Name: %s\n", course->name);
+  printf("Code: %s\n", course->code);
+  printf("Total students: %d\n\n", course->total_students);
+  printf("****************************************\n\n");
+  for (int i = 0; i < course->total_students; i++) 
+    print_student(&course->students[i]);
+}
+
+// Finds the student with the highest average
+Student* top_student(Course* course)
+{
+  if (course->total_students == 0) return NULL;
+ 
+  double student_average = 0;
+  double max_average = average(&course->students[0]);
+  Student *student = &course->students[0];
+ 
+  for (int i = 1; i < course->total_students; i++)
+  {
+    student_average = average(&course->students[i]);
+    if (student_average > max_average) 
+    {
+      max_average = student_average;
+      student = &course->students[i];
+    }   
+  }
+
+  return student;
+}
+
+// Returns the student whose average is greater than 50%
+Student *passing(Course* course, int *total_passing)
+{
+  int count = 0;
+  Student *passing = NULL;
+  
+  for (int i = 0; i < course->total_students; i++) 
+    if (average(&course->students[i]) >= 50) count++;
+  
+  passing = calloc(count, sizeof(Student));
+
+  int j = 0;
+  for (int i = 0; i < course->total_students; i++)
+  {
+    if (average(&course->students[i]) >= 50)
+    {
+      passing[j] = course->students[i];
+      j++; 
+    }
+  }
+
+  *total_passing = count;
+
+  return passing;
+}
